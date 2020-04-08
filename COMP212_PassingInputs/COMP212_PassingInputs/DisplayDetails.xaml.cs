f946818e1316380b26999b78b01bee52b15bbfaa
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-04-08 12:12:40 -0400

    added contentpage-xaml

---
+++ COMP212_PassingInputs/COMP212_PassingInputs/DisplayDetails.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace COMP212_PassingInputs
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DisplayDetails : ContentPage
+    {
+        public DisplayDetails()
+        {
+            InitializeComponent();
+        }
+    }
+}
